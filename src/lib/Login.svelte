1c327584ec2b9be12b603badf0de0d5956637b5c
CraigYeoman <yeomancraig@gmail.com>
2024-11-09 16:37:16 -0600

    Login and Sign up basic errors

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
 	import { currentUser, pb } from './pocketbase';
+	import { ClientResponseError } from 'pocketbase';
 
+	let loginToggle: boolean = false;
 	let username: string;
 	let password: string;
+	let error: string;
+
+	function toggleLogin() {
+		loginToggle = !loginToggle;
+	}
 
 	async function login() {
-		await pb.collection('users').authWithPassword(username, password);
+		error = '';
+		try {
+			await pb.collection('users').authWithPassword(username, password);
+		} catch (err) {
+			if (err instanceof ClientResponseError) {
+				error = 'Invalid username or password';
+			} else {
+				error = 'An unexpected error occurred';
+			}
+		}
 	}
 
 	async function signUp() {
@@ -18,12 +34,26 @@
 			const createdUser = await pb.collection('users').create(data);
 			await login();
 		} catch (err) {
-			console.error(err);
+			if (err instanceof ClientResponseError) {
+				console.log(err.data);
+				if (err.data?.data?.username?.code === 'validation_invalid_username') {
+					error = 'The username is invalid or already in use';
+				} else if (err.data?.data?.password?.code === 'validation_length_out_of_range') {
+					error = 'The length must be between 8 and 72.';
+				} else {
+					error = err.message;
+				}
+			} else {
+				error = 'An unexpected error occurred';
+			}
 		}
 	}
 
 	function signOut() {
 		pb.authStore.clear();
+		username = '';
+		password = '';
+		error = '';
 	}
 </script>
 
@@ -34,12 +64,21 @@
 			<button class="btn btn-accent" on:click={signOut}>Sign Out</button>
 		</p>
 	{:else}
-		<form on:submit|preventDefault>
+		<form class="flex w-32 flex-col gap-4" on:submit|preventDefault>
 			<input placeholder="Username" type="text" bind:value={username} />
 
 			<input placeholder="Password" type="password" bind:value={password} />
-			<button on:click={signUp}>Sign Up</button>
-			<button on:click={login}>Login</button>
+			{#if !loginToggle}
+				<button class="btn btn-accent" on:click={signUp}>Sign Up</button>
+				<button class="underline" on:click={toggleLogin}>Login</button>
+			{:else}
+				<button class="btn btn-accent" on:click={login}>Login</button>
+				<button class="underline" on:click={toggleLogin}>Create an account</button>
+			{/if}
 		</form>
+	{/if}
+
+	{#if error}
+		<p class="text-red-500">{error}</p>
 	{/if}
 </div>
--- src/lib/Messages.svelte
+++ src/lib/Messages.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
 	import { currentUser, pb } from './pocketbase';
-	import { format, formatDistance, formatRelative, parseISO } from 'date-fns';
+	import { format, parseISO } from 'date-fns';
 
 	let newMessage: string;
 	let messages = [];
@@ -40,39 +40,29 @@
 	}
 </script>
 
-<form on:submit|preventDefault={sendMessage}>
-	<input placeholder="Message" type="text" bind:value={newMessage} />
-	<button class="btn btn-primary" type="submit">Send</button>
-</form>
+{#if $currentUser}
+	<form on:submit|preventDefault={sendMessage}>
+		<input placeholder="Message" type="text" bind:value={newMessage} />
+		<button class="btn btn-primary" type="submit">Send</button>
+	</form>
 
-<div class="messages">
-	{#each messages as message (message.id)}
-		<div class="chat chat-start">
-			<div class="chat-image avatar">
-				<div class="w-10 rounded-full">
-					<img alt="avatar" src={`https://api.dicebear.com/9.x/bottts/svg?seed=Felix`} />
+	<div class="messages">
+		{#each messages as message (message.id)}
+			<div class="chat chat-start">
+				<div class="chat-image avatar">
+					<div class="w-10 rounded-full">
+						<img
+							alt="avatar"
+							src={`https://api.dicebear.com/9.x/bottts/svg?seed=${message.expand?.user?.username}`}
+						/>
+					</div>
 				</div>
+				<div class="chat-header">
+					@{message.expand?.user?.username}
+					<time class="text-xs opacity-50">{format(parseISO(message.created), 'PPpp')}</time>
+				</div>
+				<div class="chat-bubble">{message.text}</div>
 			</div>
-			<div class="chat-header">
-				@{message.expand?.user?.username}
-				<time class="text-xs opacity-50">{format(parseISO(message.created), 'PPpp')}</time>
-			</div>
-			<div class="chat-bubble">{message.text}</div>
-		</div>
-
-		<!-- <div class="msg">
-			<img
-				class="avatar"
-				src={`https://avatars.dicebear.com/api/identicon/${message.expand?.user?.username}.svg`}
-				alt="avatar"
-				width="40px"
-			/>
-			<div>
-				<small>
-					Sent by @{message.expand?.user?.username}
-				</small>
-				<p class="msg-text">{message.text}</p>
-			</div>
-		</div> -->
-	{/each}
-</div>
+		{/each}
+	</div>
+{/if}
